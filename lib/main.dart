38ee0dba0b12b00c0080e7a82adbd799d36e2d80
Riaz <94858893+vanJefferson@users.noreply.github.com>
2022-01-21 15:51:33 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       themeMode: themeProvider.themeMode,
       theme: MyThemes.lightTheme,
-      darkTheme: MyThemes.darkTheme,
       home: LoginPage(),
     );
   }
